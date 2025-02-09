cee3ffc21ac0fe88117d4a3b8375ddc9ee5a72d2
Zachary Knoblauch <CaptainSly@users.noreply.github.com>
2025-02-09 05:28:44 -0600

    Gradle Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,27 @@ java {
         languageVersion = JavaLanguageVersion.of(21)
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"]) // Or the component type you need to publish (e.g., kotlin)
+            
+            groupId = "com.spireprod.cje" // Your group ID
+            artifactId = "console-java-engine" // The artifact ID
+            version = "0.1.5-Talos" // Version
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Reposilite"
+            url = uri("http://maven.reignleif.com/releases") // Your Reposilite URL
+            isAllowInsecureProtocol = true
+            credentials {
+                username = findProperty("repo.username") as String? ?: System.getenv("REPO_USERNAME")
+                password = findProperty("repo.password") as String? ?: System.getenv("REPO_PASSWORD")
+            }
+        }
+    }
+}
