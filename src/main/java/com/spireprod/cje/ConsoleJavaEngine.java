e66f86c3999b49341dade9991687fddbe4125b57
Zachary Knoblauch <CaptainSly@users.noreply.github.com>
2025-07-19 00:44:52 -0500

    Fixed Window not closing, and changed game loop

--- src/main/java/com/spireprod/cje/ConsoleJavaEngine.java
+++ src/main/java/com/spireprod/cje/ConsoleJavaEngine.java
@@ -4,6 +4,8 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
@@ -26,30 +28,37 @@ public abstract class ConsoleJavaEngine {
 	protected Terminal terminal;
 	protected TextGraphics termGraphics;
 	protected KeyStroke termKey;
+	protected SwingTerminalFrame frame;
 	protected Screen screen;
 	protected boolean isRunning = false;
 
 	private int termWidth, termHeight;
 
-	private int targetFPS = 60;
+	private final int targetFPS = 60;
+	private final long optimalTime = (long) (1E9f / targetFPS);
 
 	public static final String CJE_VERSION = "0.1.5-Talos";
 
 	public ConsoleJavaEngine(String title, int width, int height) {
 		DefaultTerminalFactory defaultTermFactory = new DefaultTerminalFactory();
-		defaultTermFactory.setTerminalEmulatorTitle(title);
-		defaultTermFactory.setInitialTerminalSize(new TerminalSize(width, height));
+		defaultTermFactory.setTerminalEmulatorTitle(title).setInitialTerminalSize(new TerminalSize(width, height))
+				.setForceAWTOverSwing(false).setPreferTerminalEmulator(true);
 
 		try {
-			terminal = defaultTermFactory.createTerminal();
+			terminal = defaultTermFactory.createSwingTerminal();
+			frame = (SwingTerminalFrame) terminal;
+			frame.setVisible(true);
+			frame.pack();
+			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
 			screen = new TerminalScreen(terminal);
 
-			this.termWidth = terminal.getTerminalSize().getColumns();
-			this.termHeight = terminal.getTerminalSize().getRows();
+			this.termWidth = frame.getTerminalSize().getColumns();
+			this.termHeight = frame.getTerminalSize().getRows();
 
 			this.termGraphics = screen.newTextGraphics();
 
-			terminal.addResizeListener(new TerminalResizeListener() {
+			frame.addResizeListener(new TerminalResizeListener() {
 
 				@Override
 				public void onResized(Terminal terminal, TerminalSize newSize) {
@@ -59,7 +68,7 @@ public abstract class ConsoleJavaEngine {
 
 			});
 
-			((SwingTerminalFrame) terminal).addWindowListener(new WindowListener() {
+			frame.addWindowListener(new WindowListener() {
 
 				@Override
 				public void windowOpened(WindowEvent e) {
@@ -67,6 +76,7 @@ public abstract class ConsoleJavaEngine {
 
 				@Override
 				public void windowClosing(WindowEvent e) {
+
 					isRunning = false;
 					try {
 						screen.close();
@@ -110,33 +120,35 @@ public abstract class ConsoleJavaEngine {
 
 	protected abstract void onGameInput(float deltaTime, KeyStroke keyStroke);
 
-	protected abstract void onGameRender(float alpha);
+	protected abstract void onGameRender(float deltaTime);
 
 	public void run() {
 
 		isRunning = true;
-		try {
-			loop();
-			screen.close();
-			terminal.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		Thread gameLoop = new Thread(() -> {
+			try {
+				loop();
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+
+		gameLoop.start();
 	}
 
-	private void loop() throws IOException {
+	private void loop() throws IOException, InterruptedException {
+		long lastLoopTime = System.nanoTime();
 
 		screen.startScreen();
 		screen.setCursorPosition(null);
 
-		final float targetDelta = 1f / targetFPS;
-		float accumulator = 0f;
-		long lastTime = System.nanoTime();
-
 		while (isRunning) {
 			long now = System.nanoTime();
-			float deltaTime = (now - lastTime) / 1E9f;
-			lastTime = now;
+			long updateLength = now - lastLoopTime;
+			lastLoopTime = now;
+
+			float delta = updateLength / ((float) optimalTime);
 
 			TerminalSize newSize = screen.doResizeIfNecessary();
 			if (newSize != null) {
@@ -144,28 +156,59 @@ public abstract class ConsoleJavaEngine {
 				this.termHeight = newSize.getRows();
 			}
 
-			accumulator += deltaTime;
+			termKey = screen.pollInput();
 
-			while (accumulator >= targetDelta) {
-				termKey = screen.pollInput();
+			if (termKey != null && termKey.getKeyType().equals(KeyType.Escape))
+				isRunning = false;
 
-				if (termKey != null && termKey.getKeyType().equals(KeyType.Escape))
-					isRunning = false;
+			if (termKey != null)
+				onGameInput(delta, termKey);
 
-				if (termKey != null)
-					onGameInput(targetDelta, termKey);
+			onGameUpdate(delta);
 
-				onGameUpdate(targetDelta);
-
-				accumulator -= targetDelta;
-			}
-
-			onGameRender(accumulator / targetDelta);
+			onGameRender(delta);
 			screen.refresh();
+
+			long sleepNanos = (lastLoopTime - System.nanoTime() + optimalTime);
+			if (sleepNanos > 0) {
+				long millis = sleepNanos / 1_000_000;
+				int nanos = (int) (sleepNanos % 1_000_000);
+				Thread.sleep(millis, nanos);
+			}
 		}
 
 		screen.stopScreen();
 	}
+
+//	private void loop() throws IOException {
+//
+//		screen.startScreen();
+//		screen.setCursorPosition(null);
+//
+//		final float targetDelta = 1f / targetFPS;
+//		float accumulator = 0f;
+//		long lastTime = System.nanoTime();
+//
+//		while (isRunning) {
+//			long now = System.nanoTime();
+//			float deltaTime = (now - lastTime) / 1E9f;
+//			lastTime = now;
+//
+
+//
+//			accumulator += deltaTime;
+//
+//			while (accumulator >= targetDelta) {
+
+//
+//				accumulator -= targetDelta;
+//			}
+//
+
+//		}
+//
+//		screen.stopScreen();
+//	}
 
 	// -----------------------------------
 	// Getters and Setters
@@ -176,10 +219,6 @@ public abstract class ConsoleJavaEngine {
 
 	public int getTerminalHeight() {
 		return termHeight;
-	}
-
-	public void setTargetFps(int fps) {
-		this.targetFPS = fps;
 	}
 
 	// -----------------------------------
